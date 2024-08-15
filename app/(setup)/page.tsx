cc641ce5d317bce2bd4d573a0addd78d7b75e3ab
satara2358 <25549072+satara2358@users.noreply.github.com>
2024-08-14 21:19:37 -0500

    prisma studio  update Prisma schema and redirect

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,36 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+
+
+const SetupPage = async () => {
+  const profile = await initialProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id
+        }
+      }
+    }
+  })
+
+  if (server) {
+    return redirect(`/server/${server.id}`)
+  }
+
+  return (
+    <div>
+     <UserButton />
+     <ModeToggle/>
+     <p>cualquier cosa</p>
+   </div>
+  );
+}
+
+export default SetupPage;
