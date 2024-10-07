d8dfe7c36cddf2ff76847e8c14c7ae920c3ea543
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-10-07 18:58:34 -0500

    Fix Eslint - Create Servers

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+  ],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Aquí puedes agregar reglas personalizadas
+  },
+};
